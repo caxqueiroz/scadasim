15b68f9d48638846ea82f3ef1c25801347bdefa5
Carlos Queiroz <caxqueiroz@gmail.com>
2012-03-11 11:48:44 +1100

    new branch to test modbus-based sim.

--- src/base/ConnectionManager.h
+++ src/base/ConnectionManager.h
@@ -22,39 +22,38 @@ class ConnectionManager;
  * The variable ratio is for future use in order to give some servers a higher
  * selection probability (achieves priority support).
  */
-struct serverConnection
-{
-	IPvXAddress address;
-	int port;
-	int profileID;
-	// Used only for priorities
-	int ratio;
-	// Module ID
-	int id;
-	// Selection probability of this server within its profile (currently uniformly distributed)
-	// Could be used, e.g. to simulate flash crowd events
-	double probability;
-	ConnectionManager *manager;
+struct serverConnection {
+    IPvXAddress address;
+    int port;
+    int profileID;
+    // Used only for priorities
+    int ratio;
+    // Module ID
+    int id;
+    // Selection probability of this server within its profile (currently uniformly distributed)
+    // Could be used, e.g. to simulate flash crowd events
+    double probability;
+    ConnectionManager *manager;
 
-	serverConnection(IPvXAddress a, int port, int profileID, int ratio, ConnectionManager *cm)
-	{
-		address = a;
-		this->port = port;
-		this->profileID = profileID;
-		this->ratio = ratio;
-		manager = cm;
-		id = -1;
-	}
+    serverConnection(IPvXAddress a, int port, int profileID, int ratio,
+            ConnectionManager *cm) {
+        address = a;
+        this->port = port;
+        this->profileID = profileID;
+        this->ratio = ratio;
+        manager = cm;
+        id = -1;
+    }
 
-	serverConnection(IPvXAddress a, int port, int profileID, int ratio, int moduleId, ConnectionManager *cm)
-	{
-		address = a;
-		this->port = port;
-		this->profileID = profileID;
-		this->ratio = ratio;
-		manager = cm;
-		this->id = moduleId;
-	}
+    serverConnection(IPvXAddress a, int port, int profileID, int ratio,
+            int moduleId, ConnectionManager *cm) {
+        address = a;
+        this->port = port;
+        this->profileID = profileID;
+        this->ratio = ratio;
+        manager = cm;
+        this->id = moduleId;
+    }
 };
 
 /**
@@ -68,25 +67,24 @@ struct serverConnection
  * The variable ratio is for future use in order to give some Cms a higher
  * selection probability (achieves priority support).
  */
-struct foreignNetwork
-{
-	ConnectionManager *manager;
-	// Used only for priorities
-	int ratio;
-	// Module ID
-	int id;
-	// Selection probability of this AS (currently uniformly distributed)
-	double probability;
-	ConnectionManager *topManager;
+struct foreignNetwork {
+    ConnectionManager *manager;
+    // Used only for priorities
+    int ratio;
+    // Module ID
+    int id;
+    // Selection probability of this AS (currently uniformly distributed)
+    double probability;
+    ConnectionManager *topManager;
 
-	foreignNetwork(ConnectionManager *c, int moduleId, int ratio, ConnectionManager *topCM)
-	{
-		manager = c;
-		this->ratio = ratio;
-		topManager = topCM;
-		this->id = moduleId;
-		this->probability = ratio;
-	}
+    foreignNetwork(ConnectionManager *c, int moduleId, int ratio,
+            ConnectionManager *topCM) {
+        manager = c;
+        this->ratio = ratio;
+        topManager = topCM;
+        this->id = moduleId;
+        this->probability = ratio;
+    }
 };
 
 //
@@ -109,84 +107,82 @@ typedef std::vector<foreignNetwork*> networkVector;
  *
  * @class ConnectionManager
  */
-class SCADASIM_API ConnectionManager: public cSimpleModule
-{
+class SCADASIM_API ConnectionManager: public cSimpleModule {
 protected:
-	/// Used by CM: Map of servers registered within own AS
-	serverMap servers;
-	/// Used by the topCM: List of all registered local connection managers
-	networkVector subCM;
-	/// Global Connection Manager
-	ConnectionManager *topCM;
-	/// State variables indicating that probability has to be re-calculated
-	/// @{
-	bool serverProbabilityIsDirty, networkProbabilityIsDirty;
-	/// }@
-	/// Indicates that this connection manager is the global one
-	bool isTopCM;
-	/// Indicates that the managed topology is flat (only true AS-level topology only)
-	bool isFlatNet;
-	/// Set traffic profile manager to request profiles
-	TrafficProfileManager *tpm;
-	/// Message necessary to indicate simulation end to connection manager
-	cMessage *endSimulationMessage;
-
+    /// Used by CM: Map of servers registered within own AS
+    serverMap servers;
+    /// Used by the topCM: List of all registered local connection managers
+    networkVector subCM;
+    /// Global Connection Manager
+    ConnectionManager *topCM;
+    /// State variables indicating that probability has to be re-calculated
+    /// @{
+    bool serverProbabilityIsDirty, networkProbabilityIsDirty;
+    /// }@
+    /// Indicates that this connection manager is the global one
+    bool isTopCM;
+    /// Indicates that the managed topology is flat (only true AS-level topology only)
+    bool isFlatNet;
+    /// Set traffic profile manager to request profiles
+    TrafficProfileManager *tpm;
+    /// Message necessary to indicate simulation end to connection manager
+    cMessage *endSimulationMessage;
 
 public:
-	ConnectionManager();
-	virtual ~ConnectionManager();
-	/// @brief Server registration with connection manager in its own AS
-	void registerServer(IPvXAddress address, int port, int profileID, int moduleId = -1);
-	/// @brief Request traffic profile and according server
-	void getServer(TargetInfo &ti, TrafficProfile &tp, int &expect, int moduleId = -1);
-	/// @brief Request server of other AS for current traffic profile
-	void getForeignServer(TargetInfo &ti, int profileId, int moduleId);
-	/// @brief Selects a server for a given traffic profile
-	bool haveProfile(TargetInfo &ti, int profileId, int moduleId = -1);
+    ConnectionManager();
+    virtual ~ConnectionManager();
+    /// @brief Server registration with connection manager in its own AS
+    void registerServer(IPvXAddress address, int port, int profileID,
+            int moduleId = -1);
+    /// @brief Request traffic profile and according server
+    void getServer(TargetInfo &ti, TrafficProfile &tp, int &expect,
+            int moduleId = -1);
+    /// @brief Request server of other AS for current traffic profile
+    void getForeignServer(TargetInfo &ti, int profileId, int moduleId);
+    /// @brief Selects a server for a given traffic profile
+    bool haveProfile(TargetInfo &ti, int profileId, int moduleId = -1);
 
-	void getServer(TargetInfo &ti, TrafficProfile &tp, int moduleId);
+    void getServer(TargetInfo &ti, TrafficProfile &tp, int moduleId);
 
 protected:
-	virtual void initialize();
-	/// @brief Stop simulation
-	virtual void handleMessage(cMessage *msg);
-	/// @brief Registers connection manager with topCM or sets it to topCM
-	void registerConnectionManager(int moduleId);
-	/// @brief Adds a local connection manager to the list of the global connection manager
-	void addSubCM(ConnectionManager *other, int moduleId);
-	/// @brief Adds a local connection manager to the list of the global connection manager
-	void addSubCM(foreignNetwork *network);
-	/// @brief Normalization of probabilities if new server is added
-	void updateServerProbabilities();
-	/// @brief Normalization of probabilities if new AS is added
-	void updateNetworkProbabilities();
-	/// @brief Updates the description within the INET GUI
-	void updateDisplay();
+    virtual void initialize();
+    /// @brief Stop simulation
+    virtual void handleMessage(cMessage *msg);
+    /// @brief Registers connection manager with topCM or sets it to topCM
+    void registerConnectionManager(int moduleId);
+    /// @brief Adds a local connection manager to the list of the global connection manager
+    void addSubCM(ConnectionManager *other, int moduleId);
+    /// @brief Adds a local connection manager to the list of the global connection manager
+    void addSubCM(foreignNetwork *network);
+    /// @brief Normalization of probabilities if new server is added
+    void updateServerProbabilities();
+    /// @brief Normalization of probabilities if new AS is added
+    void updateNetworkProbabilities();
+    /// @brief Updates the description within the INET GUI
+    void updateDisplay();
 };
-
-
 
 /**
  * @brief Gives access to the ConnectionManager instance of parent Autonomous System.
  *
  * @class ConnectionManagerAccess
  */
-class SCADASIM_API ConnectionManagerAccess
-{
-  public:
-    ConnectionManagerAccess() {}
-
-    ConnectionManager *get()
-    {
-	// returns global connectionManager
-    	return check_and_cast<ConnectionManager*>(simulation.getModuleByPath("connectionManager"));
+class SCADASIM_API ConnectionManagerAccess {
+public:
+    ConnectionManagerAccess() {
     }
 
-    ConnectionManager *get(std::string asName)
-    {
-	// returns connectionManager of given autonomous system
-	asName = asName.append(".connectionManager");
-    	return check_and_cast<ConnectionManager*>(simulation.getModuleByPath(asName.c_str()));
+    ConnectionManager *get() {
+        // returns global connectionManager
+        return check_and_cast<ConnectionManager*>(
+                simulation.getModuleByPath("connectionManager"));
+    }
+
+    ConnectionManager *get(std::string asName) {
+        // returns connectionManager of given autonomous system
+        asName = asName.append(".connectionManager");
+        return check_and_cast<ConnectionManager*>(
+                simulation.getModuleByPath(asName.c_str()));
     }
 };
 
