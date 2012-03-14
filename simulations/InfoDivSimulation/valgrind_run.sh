9361d516c35209dc0b6b9c5cab5fb1081818864c
Carlos Queiroz <caxqueiroz@gmail.com>
2012-03-14 16:07:18 +1100

    run scripts added for infodiv simulation.

---
+++ simulations/InfoDivSimulation/valgrind_run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+cd /Users/cax/Dev/SCADASimWorkspace/scadasim/simulations/InfoDivSimulation
+valgrind --leak-check=yes ../../src/scadasim -r 0 -u Cmdenv -n ..:../../src:../../../inet/examples:../../../inet/src -l ../../../inet/src/inet omnetpp.ini
+# for shared lib, use: opp_run -l ../src/scadasim -n .:../src $*
